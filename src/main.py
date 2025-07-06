9c808c30622b49232bbc8547121efb8b0d54d9fc
slaughtbear <ivanaguirre368@gmail.com>
2025-07-06 13:33:31 -0600

    feat: Implementación de renderizado de plantillas

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from src.routes.auth import auth_router
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="src/templates")
 
 
-@app.get("/")
-def read_root() -> dict:
-    return {'msg': 'Welcome to Drive & Deal App!'}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html",
+    )
+
+
+app.include_router(
+    router=auth_router,
+    prefix="/auth",
+    tags=["Authentication"]
+)
